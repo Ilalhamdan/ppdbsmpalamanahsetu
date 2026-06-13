849be34e7d7385b689c2aac24a0a8e4ccdbc55f7
Ilalhamdan <hamdanqodu@gmail.com>
2026-06-13 22:00:26 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,16 +1,3 @@
 <?php
-// Membuat folder penyimpanan sementara di Vercel jika belum ada
-if (isset($_SERVER['VERCEL_URL'])) {
-    $storageFolders = [
-        '/tmp/storage/framework/views',
-        '/tmp/storage/framework/cache',
-        '/tmp/storage/framework/sessions',
-        '/tmp/storage/logs'
-    ];
-    foreach ($storageFolders as $folder) {
-        if (!is_dir($folder)) {
-            mkdir($folder, 0755, true);
-        }
-    }
-}
+
 require __DIR__ . '/../public/index.php';
