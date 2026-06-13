a4c07de07d31f26713977d94c1fe52bb436da867
Ilalhamdan <hamdanqodu@gmail.com>
2026-06-13 21:46:14 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,16 @@
 <?php
-// Meneruskan request ke file public/index.php bawaan Laravel
+// Membuat folder penyimpanan sementara di Vercel jika belum ada
+if (isset($_SERVER['VERCEL_URL'])) {
+    $storageFolders = [
+        '/tmp/storage/framework/views',
+        '/tmp/storage/framework/cache',
+        '/tmp/storage/framework/sessions',
+        '/tmp/storage/logs'
+    ];
+    foreach ($storageFolders as $folder) {
+        if (!is_dir($folder)) {
+            mkdir($folder, 0755, true);
+        }
+    }
+}
 require __DIR__ . '/../public/index.php';
